644f140584bf3558dc9ee6d8a6cc88d97cd24d72
Jaimon <jaimon.jacob2009@gmail.com>
2023-05-11 11:47:10 +0530

    updates

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ fs.readFile('./txt/input.txt', 'utf-8', (err, data)=>{
     return console.log(data)
 })
 
-const http = require ('http');
 const server = http.createServer((req, res) => {
-    console.log(req)
-    res.end ("The server is listening")
-})
+    const pathName = req.url  
+  if (pathName==='/' || pathName === ('/home')){
+      res.end('hellow from home')
+  }
+  else if (pathName === ('/main')) {
+    res.end('No main available')
+  } else {
+    res.writeHead(404, {
+      'Content-type' : 'text/html'
+    })
+    res.end('<h3>Page not found</h3>')
+  }
+  
+  
+  }
+)
 
 server.listen(3000, '127.0.0.1', ()=>{
  console.log("listening to connections on teh server")
