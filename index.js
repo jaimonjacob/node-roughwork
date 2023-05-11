22c7412cd6cb0efd4b689459c6752db8d153fec3
jaimonjacob <jaimon.jacob2009@gmail.com>
2023-05-11 04:41:29 +0000

    df

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
+const http = require ('http');
+const url = require ('url');
+
+
+
 fs.readFile('./txt/input.txt', 'utf-8', (err, data)=>{
     return console.log(data)
 })
 
-const asyncRead = async function(){
-  const firstDoc = 
-}
+const http = require ('http');
+const server = http.createServer((req, res) => {
+    console.log(req)
+    res.end ("The server is listening")
+})
+
+server.listen(3000, '127.0.0.1', ()=>{
+ console.log("listening to connections on teh server")
+})
