56e9fbc309705a3bc9687e7646849e27633d9404
jaimonjacob <jaimon.jacob2009@gmail.com>
2023-05-13 13:33:22 +0000

    Updates

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const fs = require('fs');
 const http = require ('http');
 const url = require ('url');
 
-
-
-fs.readFile('./txt/input.txt', 'utf-8', (err, data)=>{
-    return console.log(data)
-})
+const data = fs.readFileSync('./dev-data/data.json');
+const apiData = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
     const pathName = req.url  
   if (pathName==='/' || pathName === ('/home')){
       res.end('hellow from home')
+  }
+  else if (pathName === ('/api')) {
+    res.writeHead('200', {
+      'Content-type' : 'application/json'
+    })
+    console.log(__dirname)
+    console.log(req.url)
+    res.end(data)
   }
   else if (pathName === ('/main')) {
     res.end('No main available')
