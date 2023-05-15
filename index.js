230d1d534d1df6fbc023cc5468e0eafb094ca665
Jaimon <jaimon.jacob2009@gmail.com>
2023-05-15 14:45:29 +0530

    update

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
 const fs = require('fs');
 const http = require ('http');
 const url = require ('url');
-const data = fs.readFileSync('./dev-data/data.json');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const apiData = JSON.parse(data)
+const tempOverview = fs.readFileSync(`${__dirname}/templates/temp_overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/temp_card.html`, 'utf-8');
+const tempDetail = fs.readFileSync(`${__dirname}/templates/temp_product.html`, 'utf-8');
 
+const replaceTemp = (temp, product) =>{
+  let output = temp.replace(/{{PRODUCTNAME}}/g , product.productName);
+  output = output.replace(/{{PRODUCTIMAGE}}/g , product.image);
+  output = output.replace(/{{PRODUCTFROM}}/g , product.from);
+  output = output.replace(/{{PRODUCTNUTRIENTS}}/g , product.nutrients);
+  output = output.replace(/{{PRODUCTDESCRIPTION}}/g , product.description);
+  output = output.replace(/{{PRODUCTQUANTITY}}/g , product.quantity);
+  output = output.replace(/{{PRODUCTPRICE}}/g , product.price);
+  output = output.replace(/{{PRODUCTLINK}}/g , product.id);
+  if (!product.organic) output = output.replace(/{{NOTORGANIC}}/g, 'not-organic');
+  return output;
+} 
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url  
-  if (pathName==='/' || pathName === ('/home')){
+    
+ const {query, pathname} = url.parse(req.url, true);
+
+
+  if (pathname==='/' || pathname === ('/home')){
       res.end('hellow from home')
   }
-  else if (pathName === ('/api')) {
+  else if (pathname === ('/api')) {
     res.writeHead('200', {
       'Content-type' : 'application/json'
     })
-    console.log(__dirname)
-    console.log(req.url)
     res.end(data)
   }
-  else if (pathName === ('/main')) {
-    res.end('No main available')
-  } else {
+  else if (pathname === ('/overview')) {
+    // PRODUCT OVERVIEW PAGE
+    const cardsHTML = apiData.map(el => replaceTemp(tempCard, el)).join(''); 
+    const output = tempOverview.replace(/{{PRODUCTOVERVIEW}}/g, cardsHTML);
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    })
+    res.end(output)
+  }
+  else if (pathname === ('/product')) {
+    //PRODUCT DETAIL PAGE
+    res.writeHead('200', {
+      'Content-type' : 'text/html'
+    })
+    const product = apiData[query.id]
+    const output = replaceTemp(tempDetail, product)
+    console.log(product)
+    res.end(output)
+  }
+  else {
     res.writeHead(404, {
       'Content-type' : 'text/html'
     })
     res.end('<h3>Page not found</h3>')
   }
-  
-  
+   
   }
 )
 
